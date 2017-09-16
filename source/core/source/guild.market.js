e3b862afa52c67b7667e5d1d3d2a65e035d3270d
LiranNis <lirannis@gmail.com>
2017-09-16 17:34:56 +0300

    Made refreshing packages great again

--- source/core/source/guild.market.js
+++ source/core/source/guild.market.js
@@ -18,22 +18,77 @@ var gca_guild_market = {
 	// Layout Improvements
 	layout : {
 		improve : function(){
-			// Change price to 1
-			document.getElementById('preis').value = "1";
+			var script = document.createElement('script');script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(script);
 			
-			// document.head.innerHTML += '<script type="text/javascript"> function marketDrop2(d,a){marketDrop(d,a);} </script>';
-			document.getElementById('market_sell').childNodes[1].attributes['data-request-function'].value = "marketDrop2";
-			/*function marketDrop2(d,a){
-				marketDrop(d,a);
-				document.getElementById('preis').value = "1";
-			}*/
-			// document.head.innerHTML += '<script>function marketDrop2(d,a){var c=jQuery("#sellForm");jQuery(\'[name="sellid"]\',c).val(d.data("itemId"));var b=d.data("priceGold")||0;jQuery(\'[name="preis"]\',c).val("1");calcDues()}</script>';
+			document.getElementById('sellForm').innerHTML += '<input type="button" value="Sell next" class="awesome-button " onClick="sellNext();return false">';
+			document.getElementById('sellForm').innerHTML += '<input id="cancel_all_button" type="button" value="Cancel all" class="awesome-button " onClick="cancelAll();return false">';
 			
-			// function marketDrop(d,a){var c=jQuery("#sellForm");jQuery('[name="sellid"]',c).val(d.data("itemId"));var b=d.data("priceGold")||0;jQuery('[name="preis"]',c).val("1");calcDues()};
-			//function marketDrop2(d,a){var c=jQuery("#sellForm");jQuery('[name="sellid"]',c).val(d.data("itemId"));var b=d.data("priceGold")||0;jQuery('[name="preis"]',c).val("1");calcDues()}
+			//document.getElementById('sellForm').innerHTML += '<input type="button" value="Sell all" class="awesome-button " onClick="sellAll()">'; // TODO
+			
+			// Deprecated
+			// document.getElementById('market_sell').childNodes[1].attributes['data-request-function'].value = "marketDrop2"; // Immediate sell on 1 at drop, use sell next instead.
+			// document.getElementById('sellForm').innerHTML += '<input type="button" value="Cancel next" class="awesome-button " onClick="cancelNext();return false">'; // Use cancel all instead
+			// document.getElementById('preis').value = "1"; // Change price to 1
 		}
 	}
 };
+
+/*function sellAll() {
+	// document.getElementById('sellForm').setAttribute("onsubmit", 'return false;');
+	$childrens = $("#inv").children();
+	for (i = 0; i < $childrens.length; i++) {
+		marketDrop2($childrens.eq(i), 1);
+	}
+}*/
+
+function sellNext() {
+	marketDrop2($("#inv").children(),1);
+}
+
+function cancelNext() {
+	var allitems = document.getElementById("market_item_table").getElementsByTagName('form');
+	for (var i = allitems.length - 1; i >= 0; i--) {
+		if(allitems[i].elements["cancel"]){
+			allitems[i].elements["cancel"].click();
+		}
+	}
+}
+
+function cancelAll() {
+	var allitems = document.getElementById("market_item_table").getElementsByTagName('form');
+	var myitems_data = [];
+	var cancel = "";
+	for (var i = allitems.length - 1; i >= 0; i--) {
+		if(allitems[i].elements["cancel"]){
+			myitems_data.push(allitems[i].elements["buyid"].value);
+			cancel = allitems[i].elements["cancel"].value;
+		}
+	}
+
+	var x = allitems.length;
+	var mytottalitems = myitems_data.length;
+
+	if(mytottalitems!=0){
+		cansel = encodeURIComponent(cancel);
+		document.getElementById('cancel_all_button').value = '0 / '+mytottalitems+' (0%)';
+		
+		for (var i = myitems_data.length - 1; i >= 0; i--) {
+			var httpReq = new XMLHttpRequest()
+			httpReq.open("POST", document.location.href, true);
+			httpReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			httpReq.onload = function(response){
+				var myitems = document.getElementById('cancel_all_button').value.match(/(\d+)/gi)[0]*1;
+				myitems++;
+				document.getElementById('cancel_all_button').value = myitems+' / '+mytottalitems+' ('+(Math.round(myitems/mytottalitems)*100)+'%)';
+				if(myitems==mytottalitems){
+					document.location.href=document.location.href;
+				}
+			}
+			
+			httpReq.send('buyid='+myitems_data[i]+'&cancel='+cancel);
+		}
+	}
+}
 
 function marketDrop2(d,a){
 	marketDrop(d,a);
